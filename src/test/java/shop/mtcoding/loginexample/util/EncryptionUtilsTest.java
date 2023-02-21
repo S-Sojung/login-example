ef4de4c823e2e5bde335b4fa1276b0fa5b523718
sojung <sojung528@gmail.com>
2023-02-21 10:28:45 +0900

    SHA256 Util 생성

---
+++ src/test/java/shop/mtcoding/loginexample/util/EncryptionUtilsTest.java
@@ -0,0 +1,16 @@
+package shop.mtcoding.loginexample.util;
+
+import org.junit.jupiter.api.Test;
+
+public class EncryptionUtilsTest {
+
+    @Test
+    public void EncryptionUtils_test() throws Exception {
+        String a1 = EncryptionUtils.encryptSHA256("1234");
+        String a2 = EncryptionUtils.encryptSHA256("1234");
+        String a3 = EncryptionUtils.encryptSHA256("1234");
+        System.out.println("테스트 : " + a1);
+        System.out.println("테스트 : " + a2);
+        System.out.println("테스트 : " + a3);
+    }
+}
